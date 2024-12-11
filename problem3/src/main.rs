605ae1d94b5abd78d28be6c17d08013b9299aef3
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-12-11 13:51:20 +0100

    ♻️  Problem #3 optimized.

--- problem3/src/main.rs
+++ problem3/src/main.rs
@@ -3,24 +3,21 @@ fn main() {
 }
 
 fn calculate_largest_prime_factor(number: u64) -> Result<u64, String> {
-    let mut largest_factor = 1;
+    let mut largest_factor = Err("The number is a prime.".to_string());
     let mut to_divide = number;
     let mut possible_factor = 2;
     while to_divide % possible_factor == 0 {
-        largest_factor = possible_factor;
+        largest_factor = Ok(possible_factor);
         to_divide /= possible_factor;
     }
     possible_factor += 1;
-    while to_divide > 1 {
+    while possible_factor * possible_factor <= to_divide {
         while to_divide % possible_factor == 0 {
-            largest_factor = possible_factor;
+            largest_factor = Ok(possible_factor);
             to_divide /= possible_factor;
         }
         possible_factor += 2;
     }
-    if largest_factor == 1 {
-        Err("Number is prime".to_string())
-    } else {
-        Ok(largest_factor)
-    }
+    largest_factor
 }
+
