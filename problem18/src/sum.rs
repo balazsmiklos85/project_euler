19a43f7061b76063056bd90084d6ece3cff95598
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-12-14 16:50:55 +0100

    ♻️ Variable name simplified

--- problem18/src/sum.rs
+++ problem18/src/sum.rs
@@ -7,27 +7,27 @@ impl Sum {
         Sum { numbers }
     }
 
-    pub fn add_first(&mut self, previous_sum: &Sum) {
-        if previous_sum.len() == 0 {
+    pub fn add_first(&mut self, previous: &Sum) {
+        if previous.len() == 0 {
             return;
         }
-        self.numbers[0] += previous_sum.numbers[0];
+        self.numbers[0] += previous.numbers[0];
     }
 
-    pub fn add_last(&mut self, previous_sum: &Sum) {
+    pub fn add_last(&mut self, previous: &Sum) {
         if self.len() == 1 {
             return;
         }
         let last = self.len() - 1;
-        self.numbers[last] += previous_sum.numbers[previous_sum.len() - 1];
+        self.numbers[last] += previous.numbers[previous.len() - 1];
     }
 
-    pub fn add_middle(&mut self, previous_sum: &Sum) {
-        if previous_sum.len() < 2 {
+    pub fn add_middle(&mut self, previous: &Sum) {
+        if previous.len() < 2 {
             return;
         }
         (1..=self.len() - 2).for_each(|i| {
-            self.numbers[i] += previous_sum.numbers[i - 1].max(previous_sum.numbers[i]);
+            self.numbers[i] += previous.numbers[i - 1].max(previous.numbers[i]);
         });
     }
 
