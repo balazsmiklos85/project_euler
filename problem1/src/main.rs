f5ba3ad841e41af3233df17829839644bd92b6a1
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-01-25 08:31:50 +0100

    ♻️ Calculation made more mathematical.
    I tried to solve the same problem on Hackerrank and they were giving
    bigger numbers to handle, thus the less functional approach and the
    extra overflow protection.

--- problem1/src/main.rs
+++ problem1/src/main.rs
@@ -3,8 +3,21 @@ fn main() {
     println!("{}", result);
 }
 
-fn sum_multiples_of_3_or_5(limit: u32) -> u32 {
-    (0..limit).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
+fn sum_multiples_of_3_or_5(limit: u64) -> u64 {
+    sum_multiples(3, &limit) + sum_multiples(5, &limit) - sum_multiples(15, &limit)
+}
+
+fn sum_multiples(n: u64, limit: &u64) -> u64 {
+    if limit < &n {
+        return 0;
+    }
+
+    let biggest_divisible = limit - (limit % n);
+    let count = biggest_divisible / n;
+    if count % 2 == 0 {
+        return n * (count / 2) * (count + 1);
+    }
+    n * ((count + 1) / 2) * count
 }
 
 #[cfg(test)]
