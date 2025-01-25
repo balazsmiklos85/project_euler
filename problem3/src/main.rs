c893859f239726e7d4a34fbf1cf79862ef1ab22e
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-01-25 09:34:44 +0100

    ♻ Responsibility distributed more evenly for better understanding of concerns; condition fixed for square numbers.

--- problem3/src/main.rs
+++ problem3/src/main.rs
@@ -1,31 +1,41 @@
 fn main() {
-    let result = calculate_largest_prime_factor(600851475143);
-    match result {
-        Ok(factor) => println!("{}", factor),
-        Err(error) => println!("{}", error),
-    }
+    println!("{}", calculate_largest_prime_factor(600851475143));
 }
 
-fn calculate_largest_prime_factor(number: u64) -> Result<u64, String> {
+/// Calculates the largest prime factor of a number.
+///
+/// # Arguments
+///
+/// * `number` - The number to calculate the largest prime factor of.
+///
+/// # Returns
+///
+/// The largest prime factor of the number, possibly including the number itself if it is prime.
+fn calculate_largest_prime_factor(number: u64) -> u64 {
     let mut to_divide: u64;
     let mut possible_factor: u64;
-    (to_divide, possible_factor) = divide(number, 2, 1);
-    while to_divide > possible_factor * possible_factor {
-        (to_divide, possible_factor) = divide(to_divide, possible_factor, 2);
+    to_divide = divide(number, &2).max(2);
+    possible_factor = 3;
+    while to_divide >= possible_factor * possible_factor {
+        to_divide = divide(to_divide, &possible_factor).max(possible_factor);
+        possible_factor += 2;
     }
-    if to_divide == number {
-        Err("The number is a prime.".to_string())
-    } else {
-        Ok(to_divide)
-    }
+    to_divide
 }
 
-fn divide(to_divide: u64, possible_factor: u64, step: u64) -> (u64, u64) {
-    let mut to_divide = to_divide;
-    let mut possible_factor = possible_factor;
+/// Tries to divide a number by a possible factor as many times as possible.
+///
+/// # Arguments
+///
+/// * `to_divide` - The number to divide.
+/// * `possible_factor` - The possible factor to divide by.
+///
+/// # Returns
+///
+/// The number reduced by the divisions.
+fn divide(mut to_divide: u64, possible_factor: &u64) -> u64 {
     while to_divide % possible_factor == 0 {
         to_divide /= possible_factor;
     }
-    possible_factor += step;
-    (to_divide, possible_factor)
+    to_divide
 }
